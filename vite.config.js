470f51df27b096967799f5c7293263f67b007256
agtugchik <agtugchik13@gmail.com>
2024-07-08 17:45:32 +0300

    init: add project config

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __dirname = path.resolve();
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@public": path.resolve(__dirname, "public"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+});
