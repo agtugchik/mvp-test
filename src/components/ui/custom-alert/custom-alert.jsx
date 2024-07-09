4b0bf7fe305a127029b7502d6dbd8532f8c8bbb8
agtugchik <agtugchik13@gmail.com>
2024-07-09 21:28:12 +0300

    refactor custom alert

--- src/components/ui/custom-alert/custom-alert.jsx
+++ src/components/ui/custom-alert/custom-alert.jsx
@@ -1,29 +1,32 @@
 import AppContext from "@context";
 import { Alert } from "antd";
+import { useContext } from "react";
 
 const firstLetterToUppercase = (word) => word[0].toUpperCase() + word.slice(1);
 
-export const CustomAlert = () => (
-  <AppContext.Consumer>
-    {({ contextValues, resetMessage }) => (
-      <>
-        {contextValues.isActiveAlert && (
-          <Alert
-            message={`Thank you, your choice is ${
-              contextValues.event[contextValues.userChoice] || "tie"
-            } with coefficient ${
-              contextValues.userChoice === "tieTeam"
-                ? contextValues.event.tie
-                : contextValues.event[
-                    `win${firstLetterToUppercase(contextValues.userChoice)}`
-                  ]
-            }`}
-            type="success"
-            closable={true}
-            onClose={resetMessage}
-          />
-        )}
-      </>
-    )}
-  </AppContext.Consumer>
-);
+export const CustomAlert = () => {
+  const { resetMessage, contextValues } = useContext(AppContext);
+
+  setTimeout(() => resetMessage(), 3000);
+
+  return (
+    <>
+      {contextValues.isActiveAlert && (
+        <Alert
+          message={`Thank you, your choice is ${
+            contextValues.event[contextValues.userChoice] || "tie"
+          } with coefficient ${
+            contextValues.userChoice === "tieTeam"
+              ? contextValues.event.tie
+              : contextValues.event[
+                  `win${firstLetterToUppercase(contextValues.userChoice)}`
+                ]
+          }`}
+          type="success"
+          closable={true}
+          onClose={resetMessage}
+        />
+      )}
+    </>
+  );
+};
