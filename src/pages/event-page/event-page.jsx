2ca493dcd52908a83b7d4156cc28b9c9d41531dc
agtugchik <agtugchik13@gmail.com>
2024-07-10 11:41:57 +0300

    refactor event page

--- src/pages/event-page/event-page.jsx
+++ src/pages/event-page/event-page.jsx
@@ -3,14 +3,15 @@ import { Page } from "@pages/page";
 import { Button, Form, Radio } from "antd";
 import { events } from "@constants/events";
 import moment from "moment";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { routes } from "../../constants/routes";
-import AppContext from "@context/";
+import AppContext from "@context";
 
 export const EventPage = () => {
   const [form] = Form.useForm();
   const navigate = useNavigate();
   const { id } = useParams();
+  const { setMessage } = useContext(AppContext);
   const [submitDisable, setSubmitDisabled] = useState(true);
   const event = events.find((event) => event.id === Number(id));
 
@@ -22,39 +23,35 @@ export const EventPage = () => {
 
   return (
     <Page>
-      <AppContext.Consumer>
-        {({ setMessage }) => (
-          <Form
-            form={form}
-            onFinish={() => {
-              onSubmitHandler();
-              setMessage(event, form.getFieldValue("radio-group"));
-            }}
-          >
-            <h2>
-              {event.firstTeam} vs {event.secondTeam}
-            </h2>
-            <h2>Round {event?.id}</h2>
-            <h3>{moment(event.date).format("MMM Do YYYY")}</h3>
-            <Form.Item name="radio-group" label="Coefficient">
-              <Radio.Group onChange={radioOnCHangeHandler}>
-                <Radio value="firstTeam">
-                  {event.firstTeam}: {event.winFirstTeam}
-                </Radio>
-                <Radio value="tieTeam">Tie: {event.tie}</Radio>
-                <Radio value="secondTeam">
-                  {event.secondTeam}: {event.winSecondTeam}
-                </Radio>
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" disabled={submitDisable}>
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        )}
-      </AppContext.Consumer>
+      <Form
+        form={form}
+        onFinish={() => {
+          onSubmitHandler();
+          setMessage(event, form.getFieldValue("radio-group"));
+        }}
+      >
+        <h2>
+          {event.firstTeam} vs {event.secondTeam}
+        </h2>
+        <h2>Round {event?.id}</h2>
+        <h3>{moment(event.date).format("MMM Do YYYY")}</h3>
+        <Form.Item name="radio-group" label="Coefficient">
+          <Radio.Group onChange={radioOnCHangeHandler}>
+            <Radio value="firstTeam">
+              {event.firstTeam}: {event.winFirstTeam}
+            </Radio>
+            <Radio value="tieTeam">Tie: {event.tie}</Radio>
+            <Radio value="secondTeam">
+              {event.secondTeam}: {event.winSecondTeam}
+            </Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" disabled={submitDisable}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </Page>
   );
 };
